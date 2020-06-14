e3d7e7ea4a052bb8ad1ff76c24c1938851615f2a
helix32 <kediasourav2@gmail.com>
2020-06-14 10:53:06 +0530

    added delete and check functionality

--- getsuka.js
+++ getsuka.js
@@ -2,6 +2,9 @@ var input = document.getElementById("userInput");
 var ul = document.querySelector("ul");
 
 //todo-list = ul
+//all event Listeners
+ul.addEventListener("click",delcheckFunc);
+input.addEventListener("keypress", addListAfterKeypress);
 
 function createLi() {
 	const todoDiv = document.createElement("div");
@@ -31,17 +34,32 @@ function createLi() {
 	
 }
 
+//delete function
+function delcheckFunc(e){
+	//console.log(e.target);   this will return the delete button tag in the console  
+	const item = e.target;
+	//deleteing the todo
+	if(item.classList[0] === "del_btn")
+	{
+		const todo = item.parentElement;
+		todo.remove();
+	}
+
+	if(item.classList[0] === "comp_btn"){
+		const todo = item.parentElement;
+		todo.classList.toggle("completed");
+	}
+}
+
 
 function enterText(){
     if(input.value.length >0)
         createLi();
-};
+}
 
 function addListAfterKeypress(event) {
     //this now looks to see if you hit "enter"/"return"
 		//the 13 is the enter key's keycode, this could also be display by event.keyCode === 13
     if(input.value.length >0 && event.which ===13)
         createLi();
-};
-
-input.addEventListener("keypress", addListAfterKeypress);
+}
